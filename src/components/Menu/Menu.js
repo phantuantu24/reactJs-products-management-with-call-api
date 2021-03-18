6eb11f6360335f9f00a4461822d5594d2e2905c9
TuPT <nhokkon2497@gmail.com>
2021-03-18 20:29:16 +0700

    finish add all router include customLink

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
 import './Menu.css';
+import { Route, Link } from 'react-router-dom'
 
+const menus = [
+  {
+    name: 'Home',
+    exact: true,
+    to: '/'
+  },
+  {
+    name: 'Product Management',
+    exact: false,
+    to: '/product-list'
+  }
+]
+
+const MyCustomLink = ({ label, to, activeOnlyWhenExact }) => {
+  return (
+    <Route path={to} exact={activeOnlyWhenExact} children={({ match }) => {
+      const active = match ? 'active' : ''
+      return (
+        <li className={active}>
+          <Link to={to}>
+            {label}
+          </Link>
+        </li>
+      )
+    }}/>
+  )
+}
+
+const showMenu = (menus) => {
+  var result = null
+  if (menus.length > 0) {
+    result = menus.map((menu, index) => (
+      <MyCustomLink 
+        key={index}
+        label={menu.name} 
+        activeOnlyWhenExact={menu.exact}
+        to={menu.to} 
+      />
+    ))
+  }
+  return result
+}
 class Menu extends Component {
+
   render() {
     return (
       <div className="navbar navbar-default">
-        <a className="navbar-brand">Call Api</a>
+        <Link className="navbar-brand" to="/">Call Api</Link>
         <ul className="nav navbar-nav">
-          <li className="active">
-            <a>Home</a>
-          </li>
-          <li>
-            <a>Product Management</a>
-          </li>
+          { showMenu(menus) }
         </ul>
       </div>
     );
