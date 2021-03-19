51c7675b50355221e9937c9df167307cc67492d9
TuPT <nhokkon2497@gmail.com>
2021-03-19 14:42:14 +0700

    integrate redux to project and test data at ProductListPage with hard data

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import ProductItem from '../../components/ProductItem/ProductItem';
 import ProductList from '../../components/ProductList/ProductList';
 import './ProductListPage.css';
@@ -20,7 +21,7 @@ class ProductListPage extends Component {
   }
 
   render() {
-    const products = []
+    const { products } = this.props
     return (
       <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <button type="button" className="btn btn-success">Add New Product</button><hr />
@@ -32,4 +33,10 @@ class ProductListPage extends Component {
   }
 }
 
-export default ProductListPage;
+const mapStateToProps = (state) => {
+  return {
+    products: state.products
+  }
+}
+
+export default connect(mapStateToProps, null)(ProductListPage);
